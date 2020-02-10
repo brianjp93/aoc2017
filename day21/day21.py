54565b401179c947c54cd80c80049d1be75ae2c0
Brian Perrett <perrettbrian@gmail.com>
2020-02-09 18:24:00 -0700

    continue day 21

--- day21/day21.py
+++ day21/day21.py
@@ -1,9 +1,11 @@
 """day21.py
 """
 import pathlib
+import numpy as np
 
 cwd = pathlib.Path(__file__).parent.absolute()
 dpath = pathlib.PurePath(cwd, 'data')
+tpath = pathlib.PurePath(cwd, 'test')
 
 STARTMAP = '''
 .#.
@@ -17,8 +19,51 @@ class Fractal:
         self.table = table
         self.m = STARTMAP
 
+    def next(self):
+        pieces = self.get_pieces()
 
-with open(dpath, 'r') as f:
+    def find_match(self, piece):
+        for i in range(4):
+            piece = self.mutate(piece, 'rotate')
+            if piece in self.table:
+                return self.table[piece]
+        piece = self.mutate(piece, 'flip')
+        for i in range(4):
+            piece = self.mutate(piece, 'rotate')
+            if piece in self.table:
+                return self.table[piece]
+
+    def get_pieces(self):
+        m = self.m.splitlines()
+        out = []
+        if len(m) % 2 == 0:
+            for y in range(0, len(m), 2):
+                lines = []
+                for x in range(0, len(m), 2):
+                    lines.append(m[y][x:x+2])
+                    lines.append(m[y+1][x:x+2])
+        elif len(m) % 3 == 0:
+            for y in range(0, len(m), 3):
+                lines = []
+                for x in range(0, len(m), 3):
+                    lines.append(m[y][x:x+3])
+                    lines.append(m[y+1][x:x+3])
+                    lines.append(m[y+2][x:x+3])
+        out.append('\n'.join(lines))
+        return out
+
+    def mutate(self, m, mutation):
+        out = []
+        for line in m.splitlines():
+            out.append([x for x in line])
+        out = np.array(out)
+        if mutation == 'flip':
+            out = np.flipud(out)
+        elif mutation == 'rotate':
+            out = np.rot90(out)
+        return '\n'.join(''.join(line) for line in out)
+
+with open(tpath, 'r') as f:
     data = f.read().splitlines()
     table = {}
     for line in data:
@@ -30,5 +75,4 @@ with open(dpath, 'r') as f:
         table[p1] = p2
 
 f = Fractal(table)
-print(f.m)
 
